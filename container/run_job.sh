33878891ca7a648b19cce333512469874a7972be
Pires, Gabriel Henrique Souto <gabriel.h.pires@accenture.com>
2024-10-10 19:00:17 -0300

    Alterei as funções de get e push pra nao ter a limitação de tamanho do arquivo

--- container/run_job.sh
+++ container/run_job.sh
@@ -3,12 +3,12 @@ mkdir -p $(dirname $EBCDIC_FILE)
 mkdir -p $(dirname $CPY_FILE)
 
 # Puxa do s3 o arquivo EBCDIC e o arquivo COPYBOOK
-aws s3api get-object --bucket $EBCDIC_BUCKET --key $EBCDIC_FILE $EBCDIC_FILE
-aws s3api get-object --bucket $EBCDIC_BUCKET --key $CPY_FILE    $CPY_FILE
+aws s3 cp s3://$EBCDIC_BUCKET/$EBCDIC_FILE $EBCDIC_FILE
+aws s3 cp s3://$EBCDIC_BUCKET/$CPY_FILE $CPY_FILE
 
 # Roda os scripts da AWS pra converter o COPYBOOK pra json e o EBCDIC pra ascii
 python3 parse_copybook_to_json.py -copybook $CPY_FILE -output $CPY_FILE.json -ebcdic $EBCDIC_FILE -ascii $EBCDIC_FILE"_ascii.csv" -recfm fb
 python3 extract_ebcdic_to_ascii.py -local-json $CPY_FILE.json
 
 # Envia o arquivo convertido pro s3
-aws s3api put-object --bucket $EBCDIC_BUCKET --key $OUTPUT_KEY"_ascii.csv" --body $EBCDIC_FILE"_ascii.csv"
+aws s3 cp $EBCDIC_FILE"_ascii.csv" s3://$EBCDIC_BUCKET/$OUTPUT_KEY"_ascii.csv"
