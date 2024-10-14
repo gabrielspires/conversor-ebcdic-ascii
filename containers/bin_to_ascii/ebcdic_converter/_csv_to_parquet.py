0a2a91de56ba96976b9fb1d4acc30f838d541ef9
Pires, Gabriel Henrique Souto <gabriel.h.pires@accenture.com>
2024-10-14 18:27:20 -0300

    script que converte o ascii pra parquet e sobe no S3

---
+++ containers/bin_to_ascii/ebcdic_converter/_csv_to_parquet.py
@@ -0,0 +1,28 @@
+import os
+import sys
+import json
+import pandas as pd
+
+
+def csv_to_parquet():
+    print("Convertendo arquivo de saída para parquet...")
+    json_file = sys.argv[1]
+
+    with open(json_file) as jf:
+        cpy_dict = json.load(jf)
+        ascii_file = cpy_dict["output"]
+
+    bucket_name = os.getenv("EBCDIC_BUCKET")
+    output_key = os.getenv("OUTPUT_KEY")
+
+    s3_url = f"s3://{bucket_name}/{output_key}.parquet"
+
+    df = pd.read_csv(ascii_file, sep="|")
+    try:
+        df.to_parquet(s3_url, index=False)
+    except Exception as e:
+        print("Erro ao salvar parquet:", e)
+
+
+if __name__ == "__main__":
+    csv_to_parquet()
