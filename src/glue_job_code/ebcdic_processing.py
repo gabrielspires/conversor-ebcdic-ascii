8927c42dc87744c20baab4ccef5a5c40e0ecae67
Pires, Gabriel Henrique Souto <gabriel.h.pires@accenture.com>
2024-10-23 20:09:17 -0300

    Código do job Spark/Glue

--- src/glue_job_code/ebcdic_processing.py
+++ src/glue_job_code/ebcdic_processing.py
@@ -9,28 +9,30 @@ from awsglue.context import GlueContext
 from awsglue.job import Job
 
 
-def processa_arquivo(arquivo: str):
+def processa_arquivo(spark_df):
+    # TODO: Implementar o processamento de cada arquivo aqui
     pass
 
 
+# Cria a sessão Spark e o contexto do job Glue
 sc = SparkContext.getOrCreate()
 glueContext = GlueContext(sc)
 spark = glueContext.spark_session
 job = Job(glueContext)
 
-# Optimize the data movement from pandas to Spark DataFrame and back
+# Otimiza a movimentação dos dados entre o Pandas e Spark
 spark.conf.set("spark.sql.execution.arrow.pyspark.enabled", "true")
 
+# Interpreta os argumentos enviados ao job
 args = getResolvedOptions(sys.argv, ["bucket", "files"])
 args["files"] = eval(args["files"])
-print(args)
+print("Bucket:", args["bucket"])
+print("Files:", args["files"])
 
-# You can define a distributed Spark DataFrame, to read the data in a distributed way and be able to process large data
-# Here it takes a bit of time because we ask it to infer schema, in practice could just let it set everything as string
-# and handle the schema manually
-
+# Lê cada arquivo no S3 e coloca em um DataFrame Spark
 for file in args["files"]:
     sdf = spark.read.parquet(f"s3://{args['bucket']}/{file}")
     print(file)
     sdf.show(n=1)
-    # TODO: Implementar o processamento de cada arquivo aqui
+
+    processa_arquivo(sdf)
